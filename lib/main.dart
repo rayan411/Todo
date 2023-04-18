7c2d5829ff8ab5af6135bae0ff55a941c2a3a970
Rayan Abdullah <ra.Olyani@gmail.com>
2023-04-18 07:37:30 +0300

    simpleEdite

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,5 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'screens/home/addtodo.dart';
-// import 'screens/home/home_screen.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//   );
-//   runApp(MyApp());
-// }
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-  
-//         primarySwatch: Colors.blue,
-//       ),
-//       home:ToDoListScreen(),
-//     );
-//   }
-// }
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-//import 'views/home.dart';
 import 'package:flutter/services.dart';
 
 import 'views/home.dart';
@@ -44,7 +16,7 @@ Future main() async {
   runApp(const ToDoApp());
 }
 
-class ToDoApp extends StatelessWidget {
+class  ToDoApp extends StatelessWidget {
   const ToDoApp({Key? key}) : super(key: key);
 
   @override
